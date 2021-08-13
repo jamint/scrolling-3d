3d8bf334e519d56c8aa8d37d2447b60a286be4d7
Jad Mintun <jadmintun@gmail.com>
2021-08-13 09:06:37 -0400

    add down arrow

--- src/animation/animation.js
+++ src/animation/animation.js
@@ -5,10 +5,12 @@ export const scrollTriggerMove = (o) => {
     o.obj,
     { x: o.fromX, y: o.fromY, z: o.fromZ },
     {
+      onStart: () => gsap.to(".arrow-down", { opacity: 0 }),
       x: o.toX,
       y: o.toY,
       z: o.toZ,
       ease: "power1.inOut",
+      onComplete: () => console.log("end move"),
       scrollTrigger: {
         trigger: o.trigger,
         scrub: 1,
